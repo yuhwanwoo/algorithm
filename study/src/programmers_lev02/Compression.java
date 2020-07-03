c5025912d32a9f46402a62f7cd773b463e3c836c
yuhwanwoo <6514687@naver.com>
2020-07-03 20:42:23 +0900

    compression comple

--- study/src/programmers_lev02/Compression.java
+++ study/src/programmers_lev02/Compression.java
@@ -2,8 +2,7 @@ package programmers_lev02;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+
 
 // https://programmers.co.kr/learn/courses/30/lessons/17684
 // 프로그래머스 17684 [3차]압축
@@ -19,21 +18,33 @@ public class Compression {
         int[] answer = {};
         HashMap<String, Integer> map=new HashMap<>();
 
-        ArrayList<String> arr=new ArrayList<>();
+        ArrayList<Integer> arr=new ArrayList<>();
         for(int i=0;i<26;i++) {
         	map.put((char)('A'+i)+"", i+1);
         }
         
         String[] msgspl=msg.split("");
-        for(int i=0;i<msgspl.length;i++) {
+        for(int i=0;i<msgspl.length;) {
             String temp=msgspl[i];
             int print=0;
+            int searchindex=0;
             while(map.containsKey(temp)) {
             	print=map.get(temp);
+            	searchindex++;
+                if(i+searchindex>msgspl.length-1)break;
+               
+                temp=temp+msgspl[i+searchindex];
+               
             }
+            arr.add(print);
+            map.put(temp, map.size()+1);
+            i+=searchindex;
         }
         
-        
+        answer=new int[arr.size()];
+        for(int i=0;i<arr.size();i++) {
+        	answer[i]=arr.get(i);
+        }
         
         return answer;
     }
