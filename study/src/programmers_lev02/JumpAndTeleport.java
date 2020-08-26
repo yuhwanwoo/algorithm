7fed29b19731cb7277ee62db200d6feff0385422
yuhwanwoo <6514687@naver.com>
2020-08-26 23:10:11 +0900

    jump init

---
+++ study/src/programmers_lev02/JumpAndTeleport.java
@@ -0,0 +1,14 @@
+package programmers_lev02;
+
+
+// https://programmers.co.kr/learn/courses/30/lessons/12980
+// 프로그래머스 12980 점프와순간이동
+
+public class JumpAndTeleport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
