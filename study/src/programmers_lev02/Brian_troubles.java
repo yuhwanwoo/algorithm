5f24efcbcd8b98718474935d311722d67078845b
yuhwanwoo <6514687@naver.com>
2020-08-28 23:17:53 +0900

    trouble init

---
+++ study/src/programmers_lev02/Brian_troubles.java
@@ -0,0 +1,16 @@
+package programmers_lev02;
+
+public class Brian_troubles {
+
+	public static void main(String[] args) {
+		String sentence="HaEaLaLaObWORLDb";
+		
+		solution(sentence);
+	}
+	
+	public static String solution(String sentence) {
+        String answer = "";
+        return answer;
+    }
+
+}
